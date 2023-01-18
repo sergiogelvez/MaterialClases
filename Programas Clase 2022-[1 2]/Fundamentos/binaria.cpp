864612b7159a8aad5451fd6dfe32d0326c31e472
Sergio Augusto Gelvez Cortes <sergio.gelvez@gmail.com>
2023-01-17 22:10:11 -0500

    Se agrega ejemplo de búsqueda binaría, suma de riemann simple

--- Programas Clase 2022-[1 2]/Fundamentos/binaria.cpp
+++ Programas Clase 2022-[1 2]/Fundamentos/binaria.cpp
@@ -1,15 +1,112 @@
 #include <iostream>
 #include <cstdlib>
+#include <cmath>
 
-/* function binary_search(A, n, T) is
-    L := 0
-    R := n − 1
-    while L ≤ R do
-        m := floor((L + R) / 2)
-        if A[m] < T then
-            L := m + 1
-        else if A[m] > T then
-            R := m − 1
-        else:
-            return m
-    return unsuccessful */
+using namespace std;
+
+int BusquedaBinaria(int *, int, int);
+
+void imprimirVec(int vec[], int n)
+{
+    int i; 
+    for (i = 0; i < n; i++)
+    {
+        cout << vec[i] << " ";
+    }
+}
+
+int BusquedaBinaria(int *A, int N, int T)
+{
+    int L = 0, R = N - 1, m;
+    while (L <= R)
+    {
+        m = floor((L + R) / 2);
+        if (A[m] < T)
+        {
+            L = m + 1;
+        } 
+        else
+            if (A[m] > T)
+            {
+                R = m - 1;    
+            }
+            else
+            {
+                return m;
+            }
+    }
+    return -1;
+}
+
+int main(int argc, char *argv[])
+{
+    int n, t;
+    srand(time(NULL));
+    if (argc > 1)
+    {
+        n = stoi(argv[1]);
+        t = stoi(argv[2]);    
+    }
+    else
+    {
+        n = 100;
+        t = 50;
+    }
+    if (n <= 1)
+        n = 100;
+    int A[n];
+    // llenado del vector
+    for (int i = 0; i < n; i++)
+    {
+        A[i] = rand() % 100;
+    }
+    cout << "Vector generado" << endl;
+    imprimirVec(A, n);
+    // ordenamiento previo
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i -1; j++)
+        {
+            if (A[j] > A[j + 1])
+            {
+                swap(A[j], A[j + 1]);
+            }
+        }
+    }
+    cout << endl;
+    cout << "Vector ordenado" << endl;
+    imprimirVec(A, n);
+    cout << endl;
+    cout << "Buscar valor " << t << " en el vector ordenado" << endl;
+    // int pos = BusquedaBinaria(A, n, t);
+    int pos = -1;
+    int L = 0, R = n - 1, m;
+    while (L <= R)
+    {
+        m = floor((L + R) / 2);
+        if (A[m] < t)
+        {
+            L = m + 1;
+        } 
+        else
+            if (A[m] > t)
+            {
+                R = m - 1;    
+            }
+            else
+            {
+                pos = m;
+                break;
+            }
+    }
+    if (pos >= 0)
+    {
+        cout << "Posición del valor en el vector: " << pos << endl;
+    }
+    else
+    {
+        cout << "No se encontró en el vector" << endl;
+    }
+    
+
+}
