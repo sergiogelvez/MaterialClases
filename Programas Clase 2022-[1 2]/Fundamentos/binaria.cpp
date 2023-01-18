c80e9a46810d3b6432b8b926de69ddb162fa1622
Sergio Augusto Gelvez Cortes <sergio.gelvez@gmail.com>
2023-01-17 20:31:13 -0500

    Se agrega ejemplo de búsqueda binaría

---
+++ Programas Clase 2022-[1 2]/Fundamentos/binaria.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+
+/* function binary_search(A, n, T) is
+    L := 0
+    R := n − 1
+    while L ≤ R do
+        m := floor((L + R) / 2)
+        if A[m] < T then
+            L := m + 1
+        else if A[m] > T then
+            R := m − 1
+        else:
+            return m
+    return unsuccessful */
