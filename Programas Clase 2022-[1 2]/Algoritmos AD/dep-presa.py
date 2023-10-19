f774cf9b0c4c27f30f882428aa3cd97887853cef
Sergio Augusto Gelvez Cortes <sergio.gelvez@gmail.com>
2023-10-19 07:39:59 -0500

    Actulizaciones a dep presa

--- Programas Clase 2022-[1 2]/Algoritmos AD/dep-presa.py
+++ Programas Clase 2022-[1 2]/Algoritmos AD/dep-presa.py
@@ -16,6 +16,8 @@ class espacio_simulacion:
         self.T_MAX = tmax
         self.campo = np.zeros((self.filas, self.cols), dtype=int)
         self.campo_prox = np.zeros((self.filas, self.cols), dtype=int)
+        
+
         i = 0
         while  i < self.max_depredadores :
             x = np.random.randint(0, self.cols)
@@ -38,9 +40,13 @@ class espacio_simulacion:
 
         self.cacerias = 0
         self.nacimientos_presas = 0
-        self.nacimientos_depredadores = 0
         self.muertes_competencia = 0
+        self.nacimientos_depredadores = 0
+        
         self.debug_depredadores = 0
+
+    def reporte_acciones(self):
+        return self.cacerias, self.nacimientos_presas, self.muertes_competencia, self.nacimientos_depredadores
 
     def dir_a_coord(self, dir):
         # direcciones:
@@ -78,62 +84,59 @@ class espacio_simulacion:
         vacios = np.where(self.campo == 0)
         return len(presas_t[0]), len(depredadores_t[0]), len(vacios[0])
     
-    def mov_presas_dir(self, dir, i, j):
-        temp_of, temp_oc = self.dir_a_coord(dir)
+    def reproduccion(self, i, j, tipo):
+        if tipo == 1 :
+            t_opuesto = 2
+            tasa_nat = self.t_nat_depredador
+        elif tipo == 2 :
+            t_opuesto = 1
+            tasa_nat = self.t_nat_presas
+        else :
+            print("Error inesperado, tipo de organismo incorrecto")
+            return 0
+        self.campo_prox[i, j] = tipo
+        p = np.random.random()
+        if p < tasa_nat :
+            pos_valida = False
+            pos_intentos = 0
+            # reproducción
+            # poner otra presa en una casilla adyacente al azar
+            while not pos_valida and pos_intentos < 8 :
+                temp_dir = np.random.randint(0, 8)
+                temp_offset = s.dir_a_coord(temp_dir)
+                #print(i + temp_offset[0], j + temp_offset[1] )
+
+                # revisar que el offset no se salga de los bordes
+                if (i + temp_offset[0] >=0 ) and (j + temp_offset[1] >=0) and (i + temp_offset[0]< self.filas) and (j + temp_offset[1] < self.cols) :
+                    if self.revisar_posicion(i + temp_offset[0], j + temp_offset[1] ) :
+                        self.campo_prox[i + temp_offset[0], j + temp_offset[1] ] = tipo
+                        pos_valida = True
+                        if tipo == 1:
+                            self.nacimientos_depredadores += 1
+                        else :
+                            self.nacimientos_presas += 1
+                    else :
+                        pos_intentos += 1
+                else :
+                    print(f"No trates de nacer fuera del campo papu - ({i + temp_offset[0]},{j + temp_offset[1]})")
+
+
+    def mov_presas(self, i, j):
+        direccion = np.random.randint(0, 8)
+        temp_of, temp_oc = self.dir_a_coord(direccion)
         # revisar bordes y existencia de otro actor
         if (i + temp_of >=0 ) and (j + temp_oc >=0) and (i + temp_of < self.filas) and (j + temp_oc < self.cols) :
             if self.campo[i + temp_of, j + temp_oc] == 0 :
                 # esto representa que se movió a una casilla válida que estaba vacía
                 self.campo_prox[i + temp_of, j + temp_oc] = 2
                 self.campo_prox[i, j] = 0
-            elif self.campo[i + temp_of, j + temp_oc] == 1 :
-                # posibilidad de caer en cacería
-                p = np.random.random()
-                if p < 1 - self.t_caza :
-                    # casa exitosa
-                    self.campo_prox[i, j] = 0
-                    self.campo[i, j] = 0
-                    self.cacerias += 1
             elif self.campo[i + temp_of, j + temp_oc] == 2 :
                 # posibilidad de reproducirse
                 self.campo_prox[i, j] = 2
-                p = np.random.random()
-                if p < self.t_nat_presas :
-                    pos_valida = False
-                    pos_intentos = 0
-                    # reproducción
-                    # poner otra presa en una casilla adyacente al azar
-                    while not pos_valida and pos_intentos < 8 :
-                        temp_dir = np.random.randint(0, 8)
-                        temp_offset = s.dir_a_coord(temp_dir)
-                        #print(i + temp_offset[0], j + temp_offset[1] )
-
-                        # revisar que el offset no se salga de los bordes
-                        if (i + temp_offset[0] >=0 ) and (j + temp_offset[1] >=0) and (i + temp_offset[0]< self.filas) and (j + temp_offset[1] < self.cols) :
-                            if self.revisar_posicion(i + temp_offset[0], j + temp_offset[1] ) :
-                                self.campo_prox[i + temp_offset[0], j + temp_offset[1] ] = 2
-                                pos_valida = True
-                                self.nacimientos_presas += 1
-                            else :
-                                pos_intentos += 1
-                        else :
-                            print(f"No trates de nacer fuera del campo papu - ({i + temp_offset[0]},{j + temp_offset[1]})")
+                self.reproduccion(i, j, 2)
         else :
             print(f"No te golpees con la pared, papu - ({i},{j})")
-            self.campo_prox[i, j] = self.campo[i, j]
-
-    def mov_presas(self, i, j):
-        # movimiento de cada actor en cada celda:
-        # reglas de prelación:
-        # izquierda > derecha, arriba > abajo, presas se mueven antes que el depredador.  Muertes ocurren antes que nacimientos.
-        # direcciones:
-        # | 0 | 1 | 2 |
-        # | 7 | x | 3 |
-        # | 6 | 5 | 4 |
-        direccion = np.random.randint(0, 8)
-        #print(f'tiempo {t}, direccion {direccion}')
-        self.mov_presas_dir(direccion, i, j)
-        
+            self.campo_prox[i, j] = 2
 
     def mov_depredadores(self, i, j):
         # movimiento de cada actor en cada celda:
@@ -165,42 +168,24 @@ class espacio_simulacion:
                     self.cacerias += 1
                     print("Muerte! depreda")
                 else :
-                    pass
+                    self.campo_prox[i, j] = 1
             elif self.campo[i + temp_of, j + temp_oc] == 1 :
                 self.debug_depredadores += 1
                 # posibilidad de reproducirse - o de matarse entre ellos
                 self.campo_prox[i, j] = 1
-                p = np.random.random()
-                if p < self.t_nat_depredador :
-                    pos_valida = False
-                    pos_intentos = 0
-                    # reproducción
-                    # poner otro depredador en una casilla adyacente al azar
-                    while not pos_valida and pos_intentos < 8 :
-                        temp_dir = np.random.randint(0, 8)
-                        temp_offset = s.dir_a_coord(temp_dir)
-                        print(i + temp_offset[0], j + temp_offset[1] )
-
-                        # revisar que el offset no se salga de los bordes
-                        if (i + temp_offset[0] >=0 ) and (j + temp_offset[1] >=0) and (i + temp_offset[0]< self.filas) and (j + temp_offset[1] < self.cols) :
-                            if self.revisar_posicion(i + temp_offset[0], j + temp_offset[1] ) :
-                                self.campo_prox[i + temp_offset[0], j + temp_offset[1] ] = 1
-                                pos_valida = True
-                                self.nacimientos_depredadores += 1
-                            else :
-                                pos_intentos += 1
-                        else :
-                            print(f"No nazca fuera del campo vago - ({i + temp_offset[0]},{j + temp_offset[1]})")
+                self.reproduccion(i, j, 1)
                 p = np.random.random()
                 if p < self.t_competencia :
                     # Me dio amsiedad por muchos depredadores!
                     self.campo_prox[i, j] = 0
-                    self.campo[i, j] = 0
+                    #self.campo[i, j] = 0
                     self.muertes_competencia += 1
                     # Acá se pone la muerte por competencia
+                else :
+                    self.campo_prox[i, j] = 1
         else :
             print(f"No se de con la pared, vago - ({i},{j})")
-            self.campo_prox[i, j] = self.campo[i, j]        
+            self.campo_prox[i, j] = 1        
 
 
 
@@ -211,13 +196,18 @@ class espacio_simulacion:
 DEP = []
 PRE = []
 VAC = []
+CAC = []
+NAP = []
+COM = []
+NAD = [] 
+        
 T = []
 
 t = 0
 n = 20
 presas = 200
 depredadores = 100
-tmax = 20
+tmax = 200
 
 s = espacio_simulacion(n, presas, depredadores, tmax)
 # creación del espacio de simulación
@@ -258,24 +248,44 @@ while t < s.T_MAX:
                 
     # contamos las presas y los depredadores.
     pre, dep, vac = s.contar_actores()
+    cac, nap, com, nad = s.reporte_acciones()
     print(f'Presas={pre} - Depredadores={dep}')
     # esperar tecla y limpiar
-    input("Press Enter to continue...")
+    #input("Press Enter to continue...")
     os.system('clear')
 
     # añadimos a las listas para mirar al final
     PRE.append(pre)
     DEP.append(dep)
     VAC.append(vac)
+
+    CAC.append(cac)
+    NAP.append(nap)
+    COM.append(com)
+    NAD.append(nad)
+
+
     T.append(t)
     # campo_prox se vuelve el actual
 
     s.campo = s.campo_prox
     t += 1
 
-print(DEP)
-print(PRE)
-print(VAC)
+print(f"Valores iniciales:")
+print(f"depredadores={depredadores}")
+print(f"presas={presas}")
+print(f"vacios={n*n - (depredadores + presas)}")
+
+print(f"Depredadores: {DEP}")
+print(f"Presas: {PRE}")
+print(f"Vacios: {VAC}")
+print(f"Cacerias(t): {CAC}")
+print(f"NacPresas(t): {NAP}")
+print(f"Competencias(t): {COM}")
+print(f"NacDepredadores(t): {NAD}")
+print(f"Tiempo: {T}")
+
+
 print(f"Muertes por cacería: {s.cacerias}")
 print(f"Nacimientos de presas: {s.nacimientos_presas}")
 print(f"Nacimientos de depredadores: {s.nacimientos_depredadores}")
