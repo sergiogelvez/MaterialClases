394ce12ae7fe77b7647103060b699b4b4405c3f8
Sergio Augusto Gelvez Cortes <sergio.gelvez@gmail.com>
2023-11-07 08:16:32 -0500

    ejemplo archivos con fstream

---
+++ Programas Clase 2022-[1 2]/Algoritmos AD/numba_cuda_matrices.py
@@ -0,0 +1,84 @@
+import numpy as np
+import numba
+from numba import cuda
+import math
+
+print(np.__version__)
+print(numba.__version__)
+
+cuda.detect()
+
+
+'''
+__global__ void MatMulKernel(Matrix A, Matrix B, Matrix C) {
+	// each thread compute one element of C
+	// by acumulating results into Cvalue
+	double Cvalue = 0;
+	int row = blockIdx.y * blockDim.y + threadIdx.y;
+	int col = blockIdx.x * blockDim.x + threadIdx.x;
+	for (int e = 0; e < A.width; ++e) {
+		Cvalue = Cvalue + A.elements[row *A.width + e] * B.elements[e * B.width + col];
+	}
+	C.elements[row * C.width + col] = Cvalue;
+}
+'''
+
+@cuda.jit
+def matmul( A, B, C, n, m, p):
+    row = cuda.threadIdx.y + cuda.blockDim.y * cuda.blockIdx.y
+    col = cuda.threadIdx.x + cuda.blockDim.x * cuda.blockIdx.x
+    if row < n and col < p:
+        cvalue = 0
+        for e in range(m):
+            cvalue = cvalue + A[row * n + e] * B[e * m + col]
+        C[row * n + col] = cvalue
+
+# Example 1.2: Add arrays
+""" @cuda.jit
+def add_array(a, b, c):
+    i = cuda.threadIdx.x + cuda.blockDim.x * cuda.blockIdx.x
+    if i < a.size:
+        c[i] = a[i] + b[i] """
+
+n = 1000
+m = 1000
+p = 1000
+
+
+a = np.random.randint(0, 10, n * m)
+b = np.random.randint(0, 10, m * p)
+#a = np.arange(N, dtype=np.float32)
+#b = np.arange(N, dtype=np.float32)
+
+dev_a = cuda.to_device(a)
+dev_b = cuda.to_device(b)
+
+dev_c = cuda.device_array(n * p)
+
+a = np.resize(a, (n, m))
+b = np.resize(b, (m, p))
+
+c = a @ b
+print(c)
+
+threadsperblock = (16, 16)
+blockspergrid_x = math.ceil(n / threadsperblock[0])
+blockspergrid_y = math.ceil(p / threadsperblock[1])
+blockspergrid = (blockspergrid_x, blockspergrid_y)
+# Note that
+#     blocks_per_grid == ceil(N / threads_per_block)
+# ensures that blocks_per_grid * threads_per_block >= N
+
+matmul[blockspergrid,  threadsperblock](dev_a, dev_b, dev_c, n, m, p)
+
+nc = dev_c.copy_to_host()
+
+
+
+nc = np.resize(nc, (n, p))
+
+print(nc)
+
+print(np.allclose(nc, c))
+
+#  True
---
+++ Programas Clase 2022-[1 2]/Fundamentos/ejemploArchivos.cpp
@@ -0,0 +1,34 @@
+// https://cplusplus.com/doc/tutorial/files/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main() {
+    fstream new_file;
+    
+    // Open a file to perform a write operation using a file object.
+    new_file.open("xyz.txt", ios::out); 
+    
+    // Checking whether the file is open.
+    if (new_file.is_open()) {
+        new_file << "My Function \n"; // Inserting text.
+        new_file.close(); // Close the file object.
+    }
+    
+    // open a file to perform read operation using file object.
+    new_file.open("xyz.txt", ios::in); 
+    
+    // Checking whether the file is open.
+    if (new_file.is_open()) { 
+        string sa;
+        // Read data from the file object and put it into a string.
+        while (getline(new_file, sa)) { 
+            // Print the data of the string.
+            cout << sa << "\n"; 
+        }
+        
+        // Close the file object.
+        new_file.close(); 
+    }
+}
