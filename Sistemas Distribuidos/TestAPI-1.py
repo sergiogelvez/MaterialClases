1b5a7c7e1724065908c1b5da97480ca9170b217d
sergiogelvez <sergio.gelvez@gmail.com>
2025-10-11 19:30:32 -0500

    Modificaciones menores a TestAPI

--- Sistemas Distribuidos/TestAPI-1.py
+++ Sistemas Distribuidos/TestAPI-1.py
@@ -2,20 +2,19 @@ import requests
 import imageio.v2 as iio
 import matplotlib.pyplot as plt
 
-url = "https://jsonplaceholder.typicode.com/users/1"
+url = "https://jsonplaceholder.typicode.com/users/"
 response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
-    print("Nombre:", data["name"])
-    print("Email:", data["email"])
+    #print(data)
+    for i,d in enumerate(data) :
+        print(f"{i}: {d}")
+    #print("Nombre:", data["name"])
+    #print("Email:", data["email"])
 else:
     print("Error:", response.status_code)
 
-
-
-
-img = iio.imread("g4g.png")
-
-plt.imshow(img)
-plt.show()
+#img = iio.imread("g4g.png")
+#plt.imshow(img)
+#plt.show()
